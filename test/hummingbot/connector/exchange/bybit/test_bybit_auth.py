2767e1b9656af2f39f74b89897edec24ea729273
isreallee82 <isreallee82@gmail.com>
2024-08-20 17:27:29 +0100

    (fix) Changes

--- test/hummingbot/connector/exchange/bybit/test_bybit_auth.py
+++ test/hummingbot/connector/exchange/bybit/test_bybit_auth.py
@@ -1,8 +1,11 @@
 import asyncio
+import hashlib
+import hmac
 from collections import OrderedDict
-from typing import Awaitable, Dict, Mapping, Optional
+from typing import Any, Awaitable, Dict, Mapping, Optional
 from unittest import TestCase
 from unittest.mock import MagicMock
+from urllib.parse import urlencode
 
 from hummingbot.connector.exchange.bybit.bybit_auth import BybitAuth
 from hummingbot.core.web_assistant.connections.data_types import RESTMethod, RESTRequest, WSJSONRequest
@@ -13,6 +16,7 @@ class BybitAuthTests(TestCase):
     def setUp(self) -> None:
         super().setUp()
         self.api_key = "testApiKey"
+        self.passphrase = "testPassphrase"
         self.secret_key = "testSecretKey"
 
         self.mock_time_provider = MagicMock()
@@ -28,21 +32,6 @@ class BybitAuthTests(TestCase):
         ret = asyncio.get_event_loop().run_until_complete(asyncio.wait_for(coroutine, timeout))
         return ret
 
-    def test_rest_auth_signature(self):
-        params = {"param_z": "value_param_z", "param_a": "value_param_a"}
-        request = RESTRequest(
-            method=RESTMethod.GET,
-            url="https://test.url/api/endpoint",
-            is_auth_required=True,
-            params=params,
-            throttler_limit_id="/api/endpoint"
-        )
-        self.async_run_with_timeout(self.auth.rest_authenticate(request))
-        self.assertEqual(request.headers["X-BAPI-API-KEY"], self.api_key)
-        self.assertIsNotNone(request.headers["X-BAPI-TIMESTAMP"])
-        sign_expected = self.auth._generate_rest_signature(request.headers["X-BAPI-TIMESTAMP"], request.method, request.params)
-        self.assertEqual(request.headers["X-BAPI-SIGN"], sign_expected)
-
     def test_add_auth_params_to_get_request_without_params(self):
         request = RESTRequest(
             method=RESTMethod.GET,
@@ -50,10 +39,13 @@ class BybitAuthTests(TestCase):
             is_auth_required=True,
             throttler_limit_id="/api/endpoint"
         )
+        params_expected = self._params_expected(request.params)
+
         self.async_run_with_timeout(self.auth.rest_authenticate(request))
-        self.assertEqual(request.headers["X-BAPI-API-KEY"], self.api_key)
-        self.assertIsNone(request.params)
-        self.assertIsNone(request.data)
+
+        self.assertEqual(params_expected['api_key'], request.params["api_key"])
+        self.assertEqual(params_expected['timestamp'], request.params["timestamp"])
+        self.assertEqual(params_expected['sign'], request.params["sign"])
 
     def test_add_auth_params_to_get_request_with_params(self):
         params = {
@@ -69,9 +61,12 @@ class BybitAuthTests(TestCase):
         )
 
         params_expected = self._params_expected(request.params)
+
         self.async_run_with_timeout(self.auth.rest_authenticate(request))
 
-        self.assertEqual(len(request.params), 2)
+        self.assertEqual(params_expected['api_key'], request.params["api_key"])
+        self.assertEqual(params_expected['timestamp'], request.params["timestamp"])
+        self.assertEqual(params_expected['sign'], request.params["sign"])
         self.assertEqual(params_expected['param_z'], request.params["param_z"])
         self.assertEqual(params_expected['param_a'], request.params["param_a"])
 
@@ -79,30 +74,39 @@ class BybitAuthTests(TestCase):
         params = {"param_z": "value_param_z", "param_a": "value_param_a"}
         request = RESTRequest(
             method=RESTMethod.POST,
-            url="https://bybit-mock/api/endpoint",
+            url="https://test.url/api/endpoint",
             data=params,
             is_auth_required=True,
             throttler_limit_id="/api/endpoint"
         )
+        params_auth = self._params_expected(request.params)
         params_request = self._params_expected(request.data)
 
         self.async_run_with_timeout(self.auth.rest_authenticate(request))
-
+        self.assertEqual(params_auth['api_key'], request.params["api_key"])
+        self.assertEqual(params_auth['timestamp'], request.params["timestamp"])
+        self.assertEqual(params_auth['sign'], request.params["sign"])
         self.assertEqual(params_request['param_z'], request.data["param_z"])
         self.assertEqual(params_request['param_a'], request.data["param_a"])
 
-    def test_ws_auth(self):
-        request = WSJSONRequest(payload={}, is_auth_required=True)
-        ws_auth_msg = self.async_run_with_timeout(self.auth.ws_authenticate(request))
+    def test_no_auth_added_to_wsrequest(self):
+        payload = {"param1": "value_param_1"}
+        request = WSJSONRequest(payload=payload, is_auth_required=True)
+        self.async_run_with_timeout(self.auth.ws_authenticate(request))
+        self.assertEqual(payload, request.payload)
 
-        api_key = ws_auth_msg["args"][0]
-        expires = ws_auth_msg["args"][1]
-        signature = ws_auth_msg["args"][2]
-
-        self.assertEqual(ws_auth_msg["op"], "auth")
-        self.assertEqual(api_key, self.api_key)
-        self.assertEqual(signature, self.auth._generate_ws_signature(expires))
+    def _generate_signature(self, params: Dict[str, Any]) -> str:
+        encoded_params_str = urlencode(params)
+        digest = hmac.new(self.secret_key.encode("utf8"), encoded_params_str.encode("utf8"), hashlib.sha256).hexdigest()
+        return digest
 
     def _params_expected(self, request_params: Optional[Mapping[str, str]]) -> Dict:
         request_params = request_params if request_params else {}
-        return OrderedDict(sorted(request_params.items(), key=lambda t: t[0]))
+        params = {
+            'timestamp': 1000000,
+            'api_key': self.api_key,
+        }
+        params.update(request_params)
+        params = OrderedDict(sorted(params.items(), key=lambda t: t[0]))
+        params['sign'] = self._generate_signature(params=params)
+        return params
