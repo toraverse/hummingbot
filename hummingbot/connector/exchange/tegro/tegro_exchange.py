e6e5ed9b90d216edfe4a0b511f85edb775691e76
isreallee82 <isreallee82@gmail.com>
2025-02-06 06:34:48 +0100

    (feat) Adjusted order fufillment state trigger

--- hummingbot/connector/exchange/tegro/tegro_exchange.py
+++ hummingbot/connector/exchange/tegro/tegro_exchange.py
@@ -447,11 +447,11 @@ class TegroExchange(ExchangePyBase):
         new_states = self.get_state(order_status)
         confirmed_state = CONSTANTS.ORDER_STATE[new_states]
         if confirmed_state == "completed":
-            self.logger().debug(f"Order {order.client_order_id} has been completed both on exchange and on chain with {order_status['quantity_filled']} filled.")
-        elif confirmed_state == "pending":
-            self.logger().debug(f"Order {order.client_order_id} has been completed on exchange and not on chain with {order_status['quantity_filled']} filled.")
-        elif confirmed_state == "partial":
-            self.logger().debug(f"Order {order.client_order_id} has been partially filled. with {order_status['quantity_filled']} filled.")
+            self.logger().info(f"Order {order.client_order_id} has been completed both on exchange and on chain with {order_status['quantity_filled']} filled.")
+        if confirmed_state == "pending":
+            self.logger().info(f"Order {order.client_order_id} has been completed on exchange and not on chain with {order_status['quantity_filled']} filled.")
+        if confirmed_state == "partial":
+            self.logger().info(f"Order {order.client_order_id} has been partially filled. with {order_status['quantity_filled']} filled.")
         order_update = OrderUpdate(
             trading_pair=order.trading_pair,
             update_timestamp=order_status["timestamp"] * 1e-3,
@@ -530,7 +530,7 @@ class TegroExchange(ExchangePyBase):
         elif state == "open" and Decimal(data["quantity_filled"]) > Decimal("0"):
             new_states = "partial"
         elif state == "closed" and Decimal(data["quantity_pending"]) > Decimal("0"):
-            new_states = "pending"
+            new_states = "completed"
         elif state == "cancelled" and data["cancel"]["code"] == 611:
             new_states = "cancelled"
         elif state == "cancelled" and data["cancel"]["code"] != 611:
@@ -551,11 +551,11 @@ class TegroExchange(ExchangePyBase):
             is_auth_required=False)
         new_states = self.get_state(updated_order_data)
         if new_states == "completed":
-            self.logger().debug(f"Order {tracked_order.client_order_id} has been completed both on exchange and on chain with {updated_order_data[0]['quantity_filled']} filled.")
-        elif new_states == "pending":
-            self.logger().debug(f"Order {tracked_order.client_order_id} has been completed on exchange and not on chain with {updated_order_data[0]['quantity_filled']} filled.")
-        elif new_states == "partial":
-            self.logger().debug(f"Order {tracked_order.client_order_id} has been partially filled. with {updated_order_data[0]['quantity_filled']} filled.")
+            self.logger().info(f"Order {tracked_order.client_order_id} has been completed both on exchange and on chain with {updated_order_data[0]['quantity_filled']} filled.")
+        if new_states == "pending":
+            self.logger().info(f"Order {tracked_order.client_order_id} has been completed on exchange and not on chain with {updated_order_data[0]['quantity_filled']} filled.")
+        if new_states == "partial":
+            self.logger().info(f"Order {tracked_order.client_order_id} has been partially filled. with {updated_order_data[0]['quantity_filled']} filled.")
         confirmed_state = CONSTANTS.ORDER_STATE[new_states]
         order_update = OrderUpdate(
             client_order_id=tracked_order.client_order_id,
